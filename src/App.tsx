26ba42206e3170998d4d4e28a67121ab1997b207
Leon <leon.suvorkov@outlook.de>
2024-01-15 11:05:49 +0100

    corrected routes to pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import {Route, Routes, useNavigate} from 'react-router-dom';
 import { Button, NextUIProvider } from '@nextui-org/react'
 import './App.css'
 import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   const navigate = useNavigate();
@@ -9,8 +11,8 @@ function App() {
     <NextUIProvider navigate={navigate}>
       <NavBar/>
       <Routes>
-        <Route path="/" element={<Button>Page1</Button>} />
-        <Route path="/about" element={<Button>Page2</Button>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
       </Routes>
     </NextUIProvider>
   )
