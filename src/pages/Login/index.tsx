9666e38fe3fee5c60a6f05f73b600cf5bb9a7162
Leo <leon.suvorkov@outlook.de>
2024-02-17 16:40:26 +0100

    fixed waves | sizes

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -6,16 +6,16 @@ import { WavyBackground } from "@/components/ui/wavy-background";
 export function Login() {
   return (
     <>
-      <div className="container relative min-h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative min-h-screen max-h-vh flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 p-0">
           <div className="hidden md:inline absolute right-4 top-4 md:right-8 md:top-8">
             <PhwtLogo/>
           </div>
-        <div className="relative hidden h-screen flex-col bg-muted p-10 text-white lg:flex dark:border-r max-w-[50vw]">
+        <div className="relative hidden h-screen flex-col bg-muted p-0 text-white lg:flex dark:border-r max-w-[50vw]">
           <div className="absolute inset-0 bg-zinc-900" />
           <div>
 
           </div>
-          <WavyBackground className="relative z-0 mb-auto">
+          <WavyBackground className="relative z-0 mb-auto" canvasClassName="max-w-[50vw]">
             <blockquote className="space-y-2">
               <p className="text-lg">
                 Dieses Projekt wurde von{" "}
@@ -32,34 +32,37 @@ export function Login() {
           </WavyBackground>
         </div>
         <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center justify-center items-center">
-              <PhwtLogo/>
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Anmleden beim Stundenplan
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Zum fortfahren Benutzerdaten angeben.
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 lg:w-[20rem]">
+            <WavyBackground className="relative z-0 my-auto" canvasClassName="md:hidden">
+              <div className="flex flex-col space-y-2 text-center justify-center items-center">
+                <PhwtLogo/>
+                <h1 className="text-2xl font-semibold tracking-tight">
+                  Anmleden beim Stundenplan
+                </h1>
+                <p className="text-sm text-muted-foreground">
+                  Zum fortfahren Benutzerdaten angeben.
+                </p>
+              </div>
+              <UserAuthForm />
+              <p className="px-8 text-center text-sm text-muted-foreground">
+                Mit dem Klicken auf Anmelden, nehmen Sie die{" "}
+                <Link
+                  href="https://ilias.phwt.de"
+                  className="underline underline-offset-4 hover:text-primary"
+                >
+                  Interne Benutzerrichtlinie 
+                </Link>{" "}
+                und{" "}
+                <Link
+                  href="https://www.phwt.de/datenschutz/"
+                  className="underline underline-offset-4 hover:text-primary"
+                >
+                  Datenschutzerklärung
+                </Link>
+                {" "} der PHWT an.
               </p>
-            </div>
-            <UserAuthForm />
-            <p className="px-8 text-center text-sm text-muted-foreground">
-              Mit dem Klicken auf Anmelden, nehmen Sie die{" "}
-              <Link
-                href="https://ilias.phwt.de"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Interne Benutzerrichtlinie 
-              </Link>{" "}
-              und{" "}
-              <Link
-                href="https://www.phwt.de/datenschutz/"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Datenschutzerklärung
-              </Link>
-              {" "} der PHWT an.
-            </p>
+            
+            </WavyBackground>
           </div>
         </div>
       </div>
